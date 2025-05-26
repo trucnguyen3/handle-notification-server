8c6a76c2f5e90103b56bc518c55894b0a54f3c96
Tr14 <xuantrucx111@gmail.com>
2025-05-26 17:44:32 +0700

    3rd commit

--- resize.js
+++ resize.js
@@ -1,13 +1,13 @@
 const sharp = require("sharp");
 
-sharp("public/push/icon.iconset/16.png")
+sharp("public/push/icon.iconset/icon_16x16.png")
   .resize(32, 32)
   .toFile("icon_16x16@2x.png");
 
-sharp("public/push/icon.iconset/32.png")
+sharp("public/push/icon.iconset/icon_32x32.png")
   .resize(64, 64)
   .toFile("icon_32x32@2x.png");
 
-sharp("public/push/icon.iconset/128.png")
+sharp("public/push/icon.iconset/icon_128x128.png")
   .resize(256, 256)
   .toFile("icon_128x128@2x.png");
