9ef19ccc65da2349dae09b931bd67e62b2cf791a
Tr14 <xuantrucx111@gmail.com>
2025-07-09 09:44:49 +0700

    webpush

---
+++ public/webpush-service-worker.js
@@ -0,0 +1,3 @@
+importScripts('https://api.pushio.com/webpush/sdk/service_min.js');
+var config = '{"appserviceKey":"BLG9jTiQRbXS04VUGFPU6pExuDqe-SxBtQOGeHPDtorrMz9i9ZYLsOyn2pe_dVTBqkZodpG2I4LNmjB0GHmvjHY=","apiKey":"ABEXFEd7Zoqi3vWPKnYTPLuj0","accountToken":"ABEtqAqQ4K843bc6hSQQTqRpA","appver":"0.0.0","apiHost":"https://vjr1qya-webpush.oraclersys.com","lazy":false}';
+orawp.init(config);
